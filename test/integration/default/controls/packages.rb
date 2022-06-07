711e619bdf8577afc7168ac29279c0ad86a40550
Eric Veiras Galisson <eric@sietch-tabr.com>
2022-06-07 14:21:17 +0200

    test: working Inspec tests

--- test/integration/default/controls/packages.rb
+++ test/integration/default/controls/packages.rb
@@ -1,19 +1,10 @@
 # frozen_string_literal: true
 
-# Prepare platform "finger"
-platform_finger = system.platform[:finger].split('.').first.to_s
-
-control 'TEMPLATE.package.install' do
+control 'carbon-relay-ng.package.install' do
   title 'The required package should be installed'
 
   # Override by `platform_finger`
-  package_name =
-    case platform_finger
-    when 'centos-6', 'amazonlinux-1'
-      'cronie'
-    else
-      'bash'
-    end
+  package_name = 'carbon-relay-ng'
 
   describe package(package_name) do
     it { should be_installed }
--- test/integration/default/controls/services.rb
+++ test/integration/default/controls/services.rb
@@ -1,19 +1,10 @@
 # frozen_string_literal: true
 
-# Prepare platform "finger"
-platform_finger = system.platform[:finger].split('.').first.to_s
-
-control 'TEMPLATE.service.running' do
+control 'carbon-relay-ng.service.running' do
   title 'The service should be installed, enabled and running'
 
   # Override by `platform_finger`
-  service_name =
-    case platform_finger
-    when 'centos-6', 'amazonlinux-1'
-      'crond'
-    else
-      'systemd-journald'
-    end
+  service_name = 'carbon-relay-ng'
 
   describe service(service_name) do
     it { should be_installed }
